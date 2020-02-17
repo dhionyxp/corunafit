5a4a40a7aba381fb82c887487938de1e6b586b12
dhionyxp <luan.diniz23@hotmail.com>
2020-02-17 07:40:45 -0300

    Quarto Commit

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+{
+    function apagartudo(){
+        document.getElementById('modalidade_ext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none";
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('capoeiraext').style.display = "none";
+        document.getElementById('boxeext').style.display = "none";
+        document.getElementById('capoeirakidext').style.display = "none";
+        document.getElementById('jiukidext').style.display = "none";
+        document.getElementById('boxekidext').style.display = "none";
+        document.getElementById('personalext').style.display = "none";
+        document.getElementById('muayext').style.display = 'none';
+    }
+    function muaythai(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('muayext').style.display = 'flex';
+        document.getElementById('grapplingext').style.display = "none";
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('capoeiraext').style.display = "none";
+        document.getElementById('boxeext').style.display = "none";
+        document.getElementById('capoeirakidext').style.display = "none";
+        document.getElementById('jiukidext').style.display = "none";
+        document.getElementById('boxekidext').style.display = "none";
+        document.getElementById('personalext').style.display = "none";
+    }
+    function fecharmodalidade(){
+        document.getElementById('modalidade_ext').style.display = 'none';
+    }
+    function jiujitsu(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('jiuext').style.display = 'flex';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function grappling(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('grapplingext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function capoeira(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('capoeiraext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function boxe(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('boxeext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function capoeirakid(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('capoeirakidext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function jiukid(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('jiukidext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function boxekid(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('boxekidext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('personalext').style.display = "none"
+    }
+    function personal(){
+        document.getElementById('modalidade_ext').style.display = 'flex';
+        document.getElementById('personalext').style.display = "flex"
+        document.getElementById('muayext').style.display = 'none';
+        document.getElementById('jiuext').style.display = 'none';
+        document.getElementById('grapplingext').style.display = "none"
+        document.getElementById('capoeiraext').style.display = "none"
+        document.getElementById('boxeext').style.display = "none"
+        document.getElementById('capoeirakidext').style.display = "none"
+        document.getElementById('jiukidext').style.display = "none"
+        document.getElementById('boxekidext').style.display = "none"
+    }
+}
